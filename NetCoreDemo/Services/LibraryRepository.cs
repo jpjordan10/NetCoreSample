8cba5c7967a5da146200db032d3c2d58ee9bcd7a
Jean Pierre Jordan <jeanpjordans10@gmail.com>
2018-02-13 16:28:12 -0500

    Net Core Demo - Setup

---
+++ NetCoreDemo/Services/LibraryRepository.cs
@@ -0,0 +1,74 @@
+﻿using NetCoreDemo.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace NetCoreDemo.Services
+{
+    public class LibraryRepository : ILibraryRepository
+    {
+        public IEnumerable<Author> GetAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author GetAuthor(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthorExists(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Book> GetBooksForAuthor(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBookForAuthor(Guid authorId, Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddBookForAuthor(Guid authorId, Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBookForAuthor(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NetCoreDemo/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NetCoreDemo.Entities;
+using NetCoreDemo.Services;
+
+namespace NetCoreDemo
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            var connectionString = Configuration["connectionStrings:libraryDBConnectionString"];
+            services.AddDbContext<LibraryContext>(o => o.UseSqlServer(connectionString));
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, LibraryContext libraryContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            else
+            {
+                app.UseExceptionHandler();
+            }
+
+            libraryContext.EnsureSeedDataForContext();
+
+            app.UseMvc();
+        }
+    }
+}
